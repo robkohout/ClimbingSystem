cd377eb3bfa6133f0de9ebc8f5f64520070cb69c
robkohout <robkohout@hotmail.com>
2025-11-14 15:37:48 -0800

    Custom Movement Component

---
+++ Source/ClimbingSystem/Private/Components/CustomMovementComponent.cpp
@@ -0,0 +1,6 @@
+﻿// Rob Kohout All Rights Reserved
+
+
+#include "Public/Components/CustomMovementComponent.h"
+
+
---
+++ Source/ClimbingSystem/Public/Components/CustomMovementComponent.h
@@ -0,0 +1,15 @@
+﻿// Rob Kohout All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "CustomMovementComponent.generated.h"
+
+
+UCLASS()
+class CLIMBINGSYSTEM_API UCustomMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+
+};
