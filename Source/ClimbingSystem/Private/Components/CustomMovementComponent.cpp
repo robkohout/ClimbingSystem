7e22cfe540e3e9580ba7305b9f6c3848099af2f9
robkohout <robkohout@hotmail.com>
2025-11-14 17:38:59 -0800

    Eye Height Trace

--- Source/ClimbingSystem/Private/Components/CustomMovementComponent.cpp
+++ Source/ClimbingSystem/Private/Components/CustomMovementComponent.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Public/Components/CustomMovementComponent.h"
+
+#include "GameFramework/Character.h"
 #include "Kismet/KismetSystemLibrary.h"
 
 #pragma region ClimbTraces
@@ -11,6 +13,7 @@ void UCustomMovementComponent::TickComponent(float DeltaTime, enum ELevelTick Ti
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 	
 	TraceForClimbableSurface();
+	TraceFromEyeHeight(100.f);
 }
 
 TArray<FHitResult> UCustomMovementComponent::DoCapsuleTraceMultiByObject(const FVector& Start, const FVector& End, bool bShowDebugShape)
@@ -33,6 +36,23 @@ TArray<FHitResult> UCustomMovementComponent::DoCapsuleTraceMultiByObject(const F
 	return OutCapsuleTraceHitResults;
 }
 
+FHitResult UCustomMovementComponent::DoLineTraceSingleByObject(const FVector& Start, const FVector& End, bool bShowDebugShape)
+{
+	FHitResult OutHit;
+	UKismetSystemLibrary::LineTraceSingleForObjects(
+		this,
+		Start,
+		End,
+		ClimbableSurfaceTraceTypes,
+		false,
+		TArray<AActor*>(),
+		bShowDebugShape ? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None,
+		OutHit,
+		false);
+	
+	return OutHit;
+}
+
 #pragma endregion
 
 #pragma region ClimbCore
@@ -42,7 +62,18 @@ void UCustomMovementComponent::TraceForClimbableSurface()
 	const FVector StartOffset = UpdatedComponent->GetForwardVector() * 30.f;
 	const FVector Start = UpdatedComponent->GetComponentLocation() + StartOffset;
 	const FVector End = Start + UpdatedComponent->GetForwardVector();
+	
 	DoCapsuleTraceMultiByObject(Start, End, true);
+}
+
+void UCustomMovementComponent::TraceFromEyeHeight(float TraceDistance, float TraceStartOffset)
+{
+	const FVector ComponentLocation = UpdatedComponent->GetComponentLocation();
+	const FVector EyeHeightOffset = UpdatedComponent->GetUpVector() * (CharacterOwner->BaseEyeHeight + TraceStartOffset);
+	const FVector Start = ComponentLocation + EyeHeightOffset;
+	const FVector End = Start + UpdatedComponent->GetForwardVector() * TraceDistance;
+	
+	DoLineTraceSingleByObject(Start, End, true);
 }
 
 #pragma endregion
